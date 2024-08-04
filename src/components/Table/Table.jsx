3e14fed699fd21c7b55d6079a0fe6547af1ee68b
turttlehouse <buidandrun464@gmail.com>
2024-08-04 21:58:11 +0545

    Pagination functionality added by embedding logic on next and previous button

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -45,6 +45,20 @@ const Table = ({ records,setRecords}) => {
     currentPage * recordsPerPage,
     (currentPage + 1) * recordsPerPage
   );
+
+  const handleNextPage = ()=>{
+    if((currentPage + 1) * recordsPerPage < records.length)
+    {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  const handlePreviousPage = ()=>{
+    if(currentPage > 0)
+    {
+      setCurrentPage(currentPage-1);
+    }
+  }
   
   return (
     <>
@@ -123,19 +137,21 @@ const Table = ({ records,setRecords}) => {
         </div>
 
         <div className="flex items-center justify-between mt-2">
-          <a href="#" className="flex items-center px-3 py-1 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
+
+          <button onClick={handlePreviousPage}   disabled={currentPage === 0} className="flex items-center px-3 py-1 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 rtl:-scale-x-100">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
             </svg>
-            <span>Previous</span>
-          </a>
+              Previous
+          </button>
 
-          <a href="#" className="flex items-center px-5 py-1 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
-            <span>Next</span>
+          <button onClick={handleNextPage} disabled={(currentPage + 1) * recordsPerPage >= records.length} className="flex items-center px-5 py-1 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
+              Next
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 rtl:-scale-x-100">
               <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
             </svg>
-          </a>
+          </button>
+
         </div>
       </section>
     </>
