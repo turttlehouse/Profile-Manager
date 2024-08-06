6f33548000715381da9e13bf891b72c9028369a8
turttlehouse <buidandrun464@gmail.com>
2024-08-06 08:58:30 +0545

    handling the case by conditional rendering when no profile is found

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -24,33 +24,46 @@ const Profiles = ({ records }) => {
       >
         Go Back
       </button>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {records.slice(0, visibleCount).map((record,index) => (
-          <div key={index} className="bg-white p-4 rounded-md shadow-xl flex flex-col items-center">
-            <img
-              src={record.profilePicture || 'default-image-path.jpg'}
-              alt={`${record.name}'s profile`}
-              className="w-32 h-32 rounded-full mb-4"
-            />
-            <h2 className="text-lg font-bold">{record.name}</h2>
-            <p className="mb-1"><strong>Email:</strong> {record.email}</p>
-            <p className="mb-1"><strong>Phone:</strong> {record.phone}</p>
-            <p className="mb-1"><strong>Date of Birth:</strong> {record.dateOfBirth}</p>
-            <p className="mb-1"><strong>City:</strong> {record.city}</p>
-            <p className="mb-1"><strong>District:</strong> {record.district}</p>
-            <p className="mb-1"><strong>Province:</strong> {record.province}</p>
-            <p className="mb-1"><strong>Country:</strong> {record.country}</p>
+
+      {
+        records && records.length > 0 ? (
+          <>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {records.slice(0, visibleCount).map((record,index) => (
+              <div key={index} className="bg-white p-4 rounded-md shadow-xl flex flex-col items-center">
+                <img
+                  src={record.profilePicture || 'default-image-path.jpg'}
+                  alt={`${record.name}'s profile`}
+                  className="w-32 h-32 rounded-full mb-4"
+                />
+                <h2 className="text-lg font-bold">{record.name}</h2>
+                <p className="mb-1"><strong>Email:</strong> {record.email}</p>
+                <p className="mb-1"><strong>Phone:</strong> {record.phone}</p>
+                <p className="mb-1"><strong>Date of Birth:</strong> {record.dateOfBirth}</p>
+                <p className="mb-1"><strong>City:</strong> {record.city}</p>
+                <p className="mb-1"><strong>District:</strong> {record.district}</p>
+                <p className="mb-1"><strong>Province:</strong> {record.province}</p>
+                <p className="mb-1"><strong>Country:</strong> {record.country}</p>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      {visibleCount < records.length && (
-        <button
-          onClick={handleLoadMore}
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-        >
-          Load More...
-        </button>
-      )}
+          {visibleCount < records.length && (
+            <button
+              onClick={handleLoadMore}
+              className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+            >
+              Load More...
+            </button>
+          )}
+        </>
+
+        )
+        :
+        (
+          <p className='text-center text-3xl font-bold'>No profiles found</p>
+        )
+      }
     </div>
   );
 };
